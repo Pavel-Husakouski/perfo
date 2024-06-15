d263f63b52be2639e7cab6e680bf5cb289b80a6f
Pauel <phusakouski@gmail.com>
2024-06-15 14:21:42 +0300

    fix throughput and rps for slow load

--- src/counter.ts
+++ src/counter.ts
@@ -1,35 +1,42 @@
 interface Chunk {
-    size: number;
-    time: number;
+  size: number;
+  time: number;
 }
 
-export function counter(period = 1000) {
-    let queue: Array<Chunk> = [];
+export function counter(period = 10000) {
+  let queue: Array<Chunk> = [];
 
-    return {read, write, reset};
+  return { read, write, reset };
 
-    function read() {
-        const now = Date.now();
-        queue = queue.filter(({time}) => now - time < period); // a linked list would be better for sure, but this is just a demo
+  function read() {
+    const now = Date.now();
+    queue = queue.filter(({ time }) => now - time < period); // a linked list would be better for sure, but this is just a demo
 
-        return {
-            throughput: queue.reduce((acc, {size}) => acc + size, 0),
-            rps: queue.length
-        }
+    if (queue.length === 0) return { throughput: 0, rps: 0 };
+
+    const start = queue[0]?.time ?? now;
+    const end = queue.slice(-1)[0]?.time ?? now;
+    const totalThroughput = queue.reduce((acc, { size }) => acc + size, 0);
+    const duration = end - start;
+
+    return {
+      throughput: totalThroughput / (duration / 1000),
+      rps: queue.length / (duration / 1000),
+    };
+  }
+
+  function write(size: number) {
+    queue.push({ size, time: Date.now() });
+
+    const first = queue[0];
+    const last = queue.slice(-1)[0];
+
+    if (last.time - first.time > period) {
+      queue.shift();
     }
+  }
 
-    function write(size: number) {
-        queue.push({size, time: Date.now()});
-
-        const first = queue[0];
-        const last = queue.slice(-1)[0];
-
-        if(last.time - first.time > period) {
-            queue.shift();
-        }
-    }
-
-    function reset() {
-        queue = [];
-    }
+  function reset() {
+    queue = [];
+  }
 }
--- test/counter.spec.ts
+++ test/counter.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import {counter} from "../lib";
+import {setTimeout} from "node:timers/promises";
 
 describe('a counter', () => {
     it('empty counter should render zero', () => {
@@ -13,18 +14,7 @@ describe('a counter', () => {
 
         c.write(1000);
 
-        expect(c.read()).to.deep.equal({throughput: 1000, rps: 1});
-    });
-
-    it('multiple calls', () => {
-        const c = counter();
-
-        c.write(1000);
-        c.write(100);
-        c.write(10);
-        c.write(1);
-
-        expect(c.read()).to.deep.equal({throughput: 1111, rps: 4});
+        expect(c.read()).to.deep.equal({throughput: Infinity, rps: Infinity});
     });
 
     it('reset', () => {
@@ -39,11 +29,12 @@ describe('a counter', () => {
         expect(c.read()).to.deep.equal({throughput: 0, rps: 0});
     });
 
-    it('multiple calls with delay', async function () {
+    it('multiple calls with delay less that a second', async function () {
         this.timeout(10000);
 
         const c = counter();
 
+        const start = Date.now();
         c.write(1000);
         await delay(500);
         c.write(1000);
@@ -51,11 +42,76 @@ describe('a counter', () => {
         c.write(1000);
         await delay(500);
         c.write(1000);
+        const end = Date.now();
 
-        expect(c.read()).to.deep.equal({throughput: 2000, rps: 2});
+        const duration = end - start;
+
+        console.log('duration', duration);
+        console.log(4000 / duration, 4 / (duration / 1000));
+
+        const { throughput, rps } = c.read();
+
+        console.log(throughput, rps);
+
+        expect(throughput).to.greaterThan(2000).and.lessThan(3000);
+        expect(rps).to.greaterThan(2).and.lessThan(3);
+    });
+
+    it('multiple calls with delay greater that a second', async function () {
+        this.timeout(10000);
+
+        const c = counter();
+
+        const start = Date.now();
+        c.write(1000);
+        await delay(1300);
+        c.write(1000);
+        await delay(1300);
+        c.write(1000);
+        await delay(1300);
+        c.write(1000);
+        const end = Date.now();
+
+        const duration = end - start;
+
+        console.log('duration', duration);
+        console.log(4000 / duration, 4 / (duration / 1000));
+
+        const { throughput, rps } = c.read();
+
+        console.log(throughput, rps);
+
+        expect(throughput).to.greaterThan(1000).and.lessThan(1500);
+        expect(rps).to.greaterThan(1).and.lessThan(1.5);
+    });
+
+    it('multiple calls with low rps', async function () {
+        this.timeout(10000);
+
+        const c = counter();
+
+        const start = Date.now();
+        c.write(1000);
+        await delay(2300);
+        c.write(1000);
+        await delay(2300);
+        c.write(1000);
+        await delay(2300);
+        c.write(1000);
+        const end = Date.now();
+
+        const duration = end - start;
+
+        console.log('duration', duration);
+        console.log(4000 / duration, 4 / (duration / 1000));
+
+        const { throughput, rps } = c.read();
+
+        console.log(throughput, rps);
+
+        expect(throughput).to.greaterThan(500).and.lessThan(1000);
+        expect(rps).to.greaterThan(0.5).and.lessThan(1);
     });
 });
 
-function delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+const delay = setTimeout;
